c074dfecbfe6095d3f1891ac2e09511fec260cc1
root <root@localhost.localdomain>
2024-11-08 16:25:58 +0530

    provisioner file uploaded

--- provisioners.tf
+++ provisioners.tf
@@ -6,7 +6,12 @@ resource "aws_vpc" "v1" {
 cidr_block = "10.0.0.0/16"
 tags = {
 Name = "myvpc"
+ }
 }
+
+resource "aws_key_pair" "pub_key" {
+public_key = file("~/.ssh/id_rsa.pub")
+key_name = "pub_key_login"
 }
 
 resource "aws_subnet" "s1" {
@@ -16,7 +21,7 @@ availability_zone = "ap-south-1a"
 map_public_ip_on_launch = true
 tags = {
 Name = "Public_subnet"
-}
+ }
 }
 
 resource "aws_subnet" "s2" {
@@ -26,14 +31,14 @@ availability_zone = "ap-south-1a"
 map_public_ip_on_launch = true
 tags = {
 Name = "Private_subnet"
-}
+ }
 }
 
 resource "aws_internet_gateway" "ig" {
 vpc_id = aws_vpc.v1.id
 tags = {
-Name = "int gt way"
-}
+Name = "IGW1"
+ }
 }
 
 resource "aws_route_table" "r1" {
@@ -44,7 +49,7 @@ gateway_id = aws_internet_gateway.ig.id
 }
 tags = {
 Name = "Pubic_Routetable"
-}
+ }
 }
 
 resource "aws_route_table_association" "ra_pub" {
@@ -52,11 +57,11 @@ subnet_id = aws_subnet.s1.id
 route_table_id = aws_route_table.r1.id
 }
 
- resource "aws_route_table" "r2" {
+resource "aws_route_table" "r2" {
 vpc_id = aws_vpc.v1.id
 tags = {
 Name = "Private_Routetable"
-}
+ }
 }
 
 resource "aws_route_table_association" "ra_pvt" {
@@ -86,7 +91,7 @@ cidr_blocks = ["0.0.0.0/0"]
 }
 tags = {
 Name = "SG1"
-}
+ }
 }
 
 resource "aws_instance" "ec1" {
@@ -94,20 +99,31 @@ ami = "ami-04a37924ffe27da53"
 instance_type = "t2.micro"
 subnet_id = aws_subnet.s1.id
 vpc_security_group_ids = [aws_security_group.sg1.id]
-key_name = "vpc_key"
+key_name = aws_key_pair.pub_key.key_name
 tags = {
-Name = "Pub_ec1"
-}
+Name = "Public_instance"
 }
 
-resource "aws_instance" "ec2" {
-ami = "ami-04a37924ffe27da53"
-instance_type = "t2.micro"
-subnet_id = aws_subnet.s2.id
-vpc_security_group_ids = [aws_security_group.sg1.id]
-key_name = "vpc_key"
-tags = {
-Name = "pvt_ec2"
+provisioner remote-exec {
+inline = [
+"sudo yum install httpd -y",
+"sudo systemctl start httpd",
+"sudo systemctl enable httpd",
+"echo 'Instance created and logged in through SSH and HTML code SUCCESSFULLY deployed' | sudo tee /var/www/html/index.html",
+"sudo systemctl restart httpd"
+]
+
+connection {
+type = "ssh"
+private_key = file("~/.ssh/id_rsa")
+user = "ec2-user"
+host = self.public_ip 
+  }
+ }
 }
-}
+
+
+
+
+
 
